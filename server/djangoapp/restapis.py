4edfb78b7a9b5dd16a76c84e0bc312d8d5270a21
Lily807 <blackheartlily807@gmail.com>
2025-05-12 12:09:51 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,15 +12,16 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
-    log=logging.getLogger('urllib3')
+    log = logging.getLogger('urllib3')
     log.setLevel(logging.DEBUG)
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
             request_url = backend_url+endpoint+"?"+params
     else:
         request_url = backend_url+endpoint
@@ -34,6 +35,7 @@ def get_request(endpoint, **kwargs):
     except Exception as err:
         print(f"Exception occurred {err=} of type {type(err)}")
 
+
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
@@ -44,9 +46,10 @@ def analyze_review_sentiments(text):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         print(f"response={response}")
-        return response.json()    
+        return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
+
 
 # def post_review(data_dict):
 # Add code for posting review
@@ -54,9 +57,10 @@ def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     print("POST from {} ".format(request_url))
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(f"response={response}")
         print(response.json())
         return response.json()
     except Exception as err:
         print(f"Exception occurred {err=} of type {type(err)}")
+        
